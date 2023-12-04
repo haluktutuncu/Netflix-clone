3015bb420d7f2802a4724ba6f778f9479bdf0e5d
Haluk Tütüncü <112336855+haluktutuncu@users.noreply.github.com>
2023-12-04 23:56:56 +0300

    Delete my-project directory

--- my-project/src/components/Bearer.jsx
+++
@@ -1,4 +0,0 @@
-const bearer =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMWYwOWFiYTlmNWQ1YmVjNjY1OGFmMjg4ZGZiNThkYyIsInN1YiI6IjY1Mjk1MzMwNjI5YjJjMDEzOWE5MWZmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mrqlt8sliwgRCTkgSla0ub4rhtI8oljltNtdkeMulUM";
-
-export default bearer;
--- my-project/src/pages/auth/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function AuthLayout() {
-  return (
-    <div className="h-full w-full flex flex-wrap overflow-auto gap-x-8 items-center justify-center ">
-      <Outlet />
-    </div>
-  );
-}
